ce99ebe52c4921aeb61c631db98f9579c0946798
Ján Plachý <jajop2003@gmail.com>
2026-06-11 19:50:50 +0200

    Improve preparation script to not rely on environment.yml

--- prepare.sh
+++ prepare.sh
@@ -2,16 +2,27 @@
 
 set -euo pipefail
 
-# Almost all dependencies are installed through conda, except for the ones listed below in this file
-if conda env list | grep 'ms-pareto-optimization' > /dev/null 2>&1; then; else conda env create --file environment.yml; fi
-conda activate ms-pareto-optimization
+ENV_NAME="ms-pareto-optimization"
 
-# Pareto optimization, matchtig count lowerbound
-make -C kmercamel
+# Almost all dependencies are installed through conda, except for the ones listed below in this file
+if conda env list | grep "$ENV_NAME" > /dev/null 2>&1; then
+    echo "Environment "$ENV_NAME" already present."
+    conda activate "$ENV_NAME"
+else
+    conda env create --name "$ENV_NAME"
+    conda activate "$ENV_NAME"
+    conda config --add channels conda-forge
+    conda config --add channels bioconda
+    conda install snakemake kmercamel xz GeCo3 matchtigs bioconda::jellyfish
+    pip install matplotlib
+fi
+
+# Pareto optimization, matchtig count lowerbound, sorted integers
+make -C kmercamel-pareto
 
 # Elias-Fano encoding
 make -C tools/EliasFano/2i_bench
 make -C tools/EliasFano
 
-# optimal-length matchtigs
+# Optimal-length matchtigs
 make -C tools/blossom5
