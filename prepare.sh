20db3e171af15b955a0658e4eb05b216326fd5e1
Ján Plachý <72157694+Jajopi@users.noreply.github.com>
2026-06-27 14:46:30 +0200

    Fix prepare.sh script and remove blossom5 from compilation

--- prepare.sh
+++ prepare.sh
@@ -2,29 +2,20 @@
 
 set -euo pipefail
 
-ENV_NAME="ms-pareto-optimization"
+# Almost all dependencies are installed through conda, except for the ones listed below in this file:
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda install GeCo3 ggcat bioconda::jellyfish kmercamel snakemake xz
+pip install matplotlib
 
-# Almost all dependencies are installed through conda, except for the ones listed below in this file
-if conda env list | grep "$ENV_NAME" > /dev/null 2>&1; then
-    echo "Environment "$ENV_NAME" already present."
-    conda activate "$ENV_NAME"
-else
-    conda env create --name "$ENV_NAME"
-    conda activate "$ENV_NAME"
-    conda config --add channels conda-forge
-    conda config --add channels bioconda
-    conda install GeCo3 ggcat bioconda::jellyfish kmercamel matchtigs snakemake xz
-    pip install matplotlib
-fi
-
-# Those dependencies are not distributed through conda, so they are included in the repository
+# Those dependencies are not distributed through conda, so they are included in the repository:
 
 # Our-work - Pareto optimization, matchtig count lowerbound, sorted integers
 make -C kmercamel-pareto
 
-# Elias-Fano encoding
-make -C tools/EliasFano/2i_bench
-make -C tools/EliasFano
-
-# Optimal-length matchtigs
-make -C tools/blossom5
+# Elias-Fano encoding for ex3
+{
+  cd tools/EliasFano/2i_bench; cmake .; cd ../../../
+  make -C tools/EliasFano/2i_bench
+  make -C tools/EliasFano
+} || true # This thing is old and weird and I was only able to get it compiled to 25%, but that's apparently enough
